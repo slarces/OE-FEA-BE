ab94be9640ee11759fdbc178f44ce049073ba8de
Thapelo Khanyile <thapelo.khanyile@xtnd.co.za>
2025-03-12 23:35:13 +0200

    policy implementation for failure resistance

--- Infrastructure/Service/CountryDataService.cs
+++ Infrastructure/Service/CountryDataService.cs
@@ -1,5 +1,6 @@
 ﻿using Flag_Explorer_App.Domain.Entities.Country;
 using Flag_Explorer_App.Dtos.Country;
+using Polly;
 using System.Text.Json;
 
 namespace Flag_Explorer_App.Infrastructure.Service
@@ -9,11 +10,31 @@ namespace Flag_Explorer_App.Infrastructure.Service
         private readonly HttpClient _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
         private readonly string _jsonFilePath = "Data/countries.json";
 
+
         public async Task FetchAndStoreCountriesAsync()
         {
+            var retryPolicy = 
+                Policy.Handle<Exception>()
+                .WaitAndRetryAsync(5,
+                    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),  // exponential backoff
+                        (exception, timeSpan, retryCount, context) =>
+                            {
+                                Console.WriteLine($"Retry {retryCount} encountered an error: {exception.Message}. Waiting {timeSpan} before next retry.");
+                            });
+
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(60));
+            _httpClient.Timeout = TimeSpan.FromSeconds(60);
+
+            var response = "";
+
             try
             {
-                var response = await _httpClient.GetStringAsync("https://restcountries.com/v3.1/all");
+                await retryPolicy.ExecuteAsync(async () =>
+                {
+                     response = await _httpClient.GetStringAsync("https://restcountries.com/v3.1/all", cts.Token);
+                });
+                
+                Console.WriteLine(response);
 
                 var countries = JsonSerializer.Deserialize<List<CountryDto>>(response, new JsonSerializerOptions
                 {
