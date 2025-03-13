2b530771edad00d3bfa2dcfed7a39032a673c032
Thapelo Khanyile <thapelo.khanyile@xtnd.co.za>
2025-03-13 02:02:32 +0200

    Global API response type T and country controller

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Flag_Explorer_App.Controllers
+{
+    public class CountryController : ControllerBase
+    {
+        private readonly ILogger<CountryController> _logger;
+        public CountryController(ILogger<CountryController> logger)
+        {
+            _logger = logger;
+        }
+
+        //countrydashboard
+
+        //getallcountries
+
+        //getcountrybyid
+
+
+    }
+}
---
+++ Models/REST/ApiResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace Flag_Explorer_App.Models.REST
+{
+    public class ApiResponse<T>(int statusCode, string statusResponse, T? data = default)
+    {
+        public int StatusCode { get; set; } = statusCode;
+
+        public string StatusResponse { get; set; } = statusResponse;
+
+        public T? Data { get; set; } = data;
+
+        public static ApiResponse<T> Success(T data, int statusCode = 200)
+        {
+            return new ApiResponse<T>(statusCode, "Success", data);
+        }
+
+        public static ApiResponse<T> Error(string message, int statusCode = 400)
+        {
+            return new ApiResponse<T>(statusCode, message, default);
+        }
+    }
+}
